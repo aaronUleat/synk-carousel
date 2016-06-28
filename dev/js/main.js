4bdfab2b91d45e60364a0e43071a2695e109273a
Aaron Ulate <aulate@mybookie.cr>
2016-06-28 11:24:41 -0600

    first commit synk carousel

---
+++ dev/js/main.js
@@ -0,0 +1,50 @@
+(function() {
+	$(function() {
+		$('.single-item').slick({
+			arrows: false
+		});
+		$('.items-carousel').slick({
+			slidesToShow: 5,
+			slidesToScroll: 1,
+			asNavFor: '.single-item',
+			dots: false,
+			centerMode: false,
+			arrows: true,
+			focusOnSelect: true,
+			responsive: [
+					{
+						breakpoint: 960,
+						settings: {
+							slidesToShow: 3,
+							slidesToScroll: 1,
+							centerMode: true,
+							centerPadding: '55px',
+							arrows: true
+						}
+					},
+					{
+						breakpoint: 768,
+				           settings: {
+								slidesToShow:2,
+								slidesToScroll: 1,
+								centerMode: true,
+								centerPadding: '55px',
+								arrows: true
+						}
+					},
+					{
+						breakpoint: 480,
+						settings: {
+							slidesToShow: 1,
+							slidesToScroll: 1,
+							dots: false,
+							centerMode: true,
+							centerPadding: '55px',
+							arrows: true
+						}
+					}
+				]
+
+		});
+	})
+})();
